5658e43b3bb97112751919c4bd47c7d3ff268901
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-06-07 15:50:05 -0500

    update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
 
     pygame.mixer.init()
     pygame.mixer.music.load("assets/connect.mp3", namehint="mp3")
-    pygame.mixer.music.play()
+
     connecting = True
 
 
@@ -58,10 +58,23 @@ def main():
             text_surface = font.render("Enter server ip and port", True, (0, 0, 0))
             screen.blit(text_surface, (0, 0))
             pygame.display.flip()
+            pygame.time.wait(1000)
             server = input("Enter server ip and wtv-1800 port (192.168.0.100:1615 as an example):")
+            pygame.mixer.music.play()
             screen.fill("black")
             screen.blit(roadImage, (0, 0))
             pygame.display.flip()
+
+
+            proxy = {
+                "http": "http://" + server + "/"
+                }
+
+            text_surface = font.render("wtv-1800:/preregister", True, (0, 0, 0))
+            screen.blit(text_surface, (0, 0))
+            pygame.display.flip()
+            wtv1800 = requests.get("wtv-1800:/preregister", proxies=proxy)
+            print(wtv1800.response)
 
 
         # End rendering
